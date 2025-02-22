0771bda28e89ebf1cc33ce9b51c6b8f28345b72d
prem-thatikonda29 <2023.tprem@isu.ac.in>
2025-02-22 16:13:36 +0530

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Login() {
+  return <section className="w-full h-screen bg-blue-50">Login</section>;
+}
+
+export default Login;
